506b3de33e4623aea11c5d0b2e2263b8f5160497
Lavkesh Lahngir <lavkesh51@gmail.com>
2022-05-09 12:06:39 +0800

    Sink connector small fixes (#6)
    * fix: depedencies and bq metada
    
    * chore: small refactoring

--- src/main/java/io/odpf/sink/connectors/bigquery/handler/NoopErrorHandler.java
+++ src/main/java/io/odpf/sink/connectors/bigquery/handler/NoopErrorHandler.java
@@ -1,16 +1,7 @@
 package io.odpf.sink.connectors.bigquery.handler;
 
-import com.google.cloud.bigquery.BigQueryError;
-import io.odpf.sink.connectors.bigquery.models.Record;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
 public class NoopErrorHandler implements ErrorHandler {
-    @Override
-    public void handle(Map<Long, List<BigQueryError>> errorInfoMap, List<Record> records) {
-
-    }
 }
