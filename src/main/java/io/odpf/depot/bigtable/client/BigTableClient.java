fb787d6ecaa45c552fbc34faecb12428ab411937
Mayur Gubrele <mayur.gubrele@gmail.com>
2022-09-26 15:19:47 +0530

    feat: Bigtable record parser (#39)
    * feat: Bigtable Sink (#33)
    
    * feat: add bigtable sink with stringified odpf messages as values
    
    * feat: add BIGTABLE_CREDENTIAL_PATH to access google cloud
    
    * feat: add wrapper class on bigtable client
    
    * refactor: fix checkstyle
    
    * feat: add bigtable parser tests
    
    * feat: add bigtable sink tests
    
    * feat: add bigtable client tests
    
    * chore: revert version bump
    
    * chore: revert version change in build.gradle
    
    Co-authored-by: mayur.gubrele <2310-mayur.gubrele@users.noreply.source.golabs.io>
    Co-authored-by: lavkesh <lavkesh51@gmail.com>
    
    * feat: create bigtable records using InputOutputFieldMapping provided as configuration
    
    * refactor: fix checkstyle and add unit tests
    
    * review: minor refactor
    
    * refactor: add BigTableSchemaTest and fix BigTableRecordParserTest
    
    * refactor: fix checkstyle
    
    * tests: add few more tests
    
    Co-authored-by: mayur.gubrele <2310-mayur.gubrele@users.noreply.source.golabs.io>
    Co-authored-by: lavkesh <lavkesh51@gmail.com>

--- src/main/java/io/odpf/depot/bigtable/client/BigTableClient.java
+++ src/main/java/io/odpf/depot/bigtable/client/BigTableClient.java
@@ -4,17 +4,22 @@ import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.bigtable.admin.v2.BigtableTableAdminClient;
 import com.google.cloud.bigtable.admin.v2.BigtableTableAdminSettings;
+import com.google.cloud.bigtable.admin.v2.models.ColumnFamily;
 import com.google.cloud.bigtable.data.v2.BigtableDataClient;
 import com.google.cloud.bigtable.data.v2.BigtableDataSettings;
 import com.google.cloud.bigtable.data.v2.models.BulkMutation;
 import com.google.cloud.bigtable.data.v2.models.MutateRowsException;
+import io.odpf.depot.bigtable.exception.BigTableInvalidSchemaException;
 import io.odpf.depot.bigtable.model.BigTableRecord;
 import io.odpf.depot.bigtable.response.BigTableResponse;
 import io.odpf.depot.config.BigTableSinkConfig;
+import org.json.JSONObject;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BigTableClient {
     private final BigtableTableAdminClient bigtableTableAdminClient;
@@ -46,13 +51,12 @@ public class BigTableClient {
                 .setInstanceId(sinkConfig.getInstanceId())
                 .setCredentialsProvider(FixedCredentialsProvider.create(GoogleCredentials.fromStream(new FileInputStream(sinkConfig.getCredentialPath()))))
                 .build();
-
         return BigtableTableAdminClient.create(settings);
     }
 
     public BigTableResponse send(List<BigTableRecord> records) throws MutateRowsException {
-        BulkMutation batch = BulkMutation.create(sinkConfig.getTableId());
         BigTableResponse bigTableResponse = null;
+        BulkMutation batch = BulkMutation.create(sinkConfig.getTableId());
         for (BigTableRecord record : records) {
             batch.add(record.getRowMutationEntry());
         }
@@ -66,8 +70,29 @@ public class BigTableClient {
         return bigTableResponse;
     }
 
-    public boolean tableExists(String tableId) {
-        return bigtableTableAdminClient.exists(tableId);
+    public void validateBigTableSchema() throws BigTableInvalidSchemaException {
+        this.tableExists(sinkConfig.getTableId());
+        this.columnFamiliesExist(sinkConfig.getColumnFamilyMapping(), sinkConfig.getTableId());
     }
 
+    private void tableExists(String tableId) throws BigTableInvalidSchemaException {
+        if (!bigtableTableAdminClient.exists(tableId)) {
+            throw new BigTableInvalidSchemaException(String.format("Table: %s does not exist", tableId));
+        }
+    }
+
+    private void columnFamiliesExist(String inputOutputFieldMapping, String tableId) throws BigTableInvalidSchemaException {
+        List<String> existingColumnFamilies = bigtableTableAdminClient.getTable(tableId)
+                .getColumnFamilies()
+                .stream()
+                .map(ColumnFamily::getId)
+                .collect(Collectors.toList());
+
+        Set<String> columnFamilies = new JSONObject(inputOutputFieldMapping).keySet();
+        for (String columnFamily : columnFamilies) {
+            if (!existingColumnFamilies.contains(columnFamily)) {
+                throw new BigTableInvalidSchemaException(String.format("Column family: %s does not exist!", columnFamily));
+            }
+        }
+    }
 }
--- src/main/java/io/odpf/depot/bigtable/parser/BigTableRecordParser.java
+++ src/main/java/io/odpf/depot/bigtable/parser/BigTableRecordParser.java
@@ -2,11 +2,14 @@ package io.odpf.depot.bigtable.parser;
 
 import com.google.cloud.bigtable.data.v2.models.RowMutationEntry;
 import io.odpf.depot.bigtable.model.BigTableRecord;
+import io.odpf.depot.bigtable.model.BigtableSchema;
+import io.odpf.depot.common.Tuple;
 import io.odpf.depot.config.BigTableSinkConfig;
 import io.odpf.depot.error.ErrorInfo;
 import io.odpf.depot.error.ErrorType;
 import io.odpf.depot.message.OdpfMessage;
 import io.odpf.depot.message.OdpfMessageParser;
+import io.odpf.depot.message.OdpfMessageSchema;
 import io.odpf.depot.message.ParsedOdpfMessage;
 import io.odpf.depot.message.SinkConnectorSchemaMessageMode;
 
@@ -17,11 +20,21 @@ public class BigTableRecordParser {
     private final BigTableSinkConfig sinkConfig;
     private final OdpfMessageParser odpfMessageParser;
     private final BigTableRowKeyParser bigTableRowKeyParser;
+    private final BigtableSchema bigTableSchema;
+    private final OdpfMessageSchema schema;
+    private final Tuple<SinkConnectorSchemaMessageMode, String> modeAndSchema;
 
-    public BigTableRecordParser(BigTableSinkConfig sinkConfig, OdpfMessageParser odpfMessageParser, BigTableRowKeyParser bigTableRowKeyParser) {
+    public BigTableRecordParser(BigTableSinkConfig sinkConfig,
+                                OdpfMessageParser odpfMessageParser,
+                                BigTableRowKeyParser bigTableRowKeyParser,
+                                Tuple<SinkConnectorSchemaMessageMode, String> modeAndSchema,
+                                OdpfMessageSchema schema) {
         this.sinkConfig = sinkConfig;
         this.odpfMessageParser = odpfMessageParser;
         this.bigTableRowKeyParser = bigTableRowKeyParser;
+        this.modeAndSchema = modeAndSchema;
+        this.bigTableSchema = new BigtableSchema(sinkConfig);
+        this.schema = schema;
     }
 
     public List<BigTableRecord> convert(List<OdpfMessage> messages) {
@@ -35,15 +48,18 @@ public class BigTableRecordParser {
     }
 
     private BigTableRecord createRecord(OdpfMessage message, long index) {
-        SinkConnectorSchemaMessageMode mode = sinkConfig.getSinkConnectorSchemaMessageMode();
-        String schemaClass = mode == SinkConnectorSchemaMessageMode.LOG_MESSAGE
-                ? sinkConfig.getSinkConnectorSchemaProtoMessageClass() : sinkConfig.getSinkConnectorSchemaProtoKeyClass();
         try {
-            ParsedOdpfMessage parsedOdpfMessage = odpfMessageParser.parse(message, mode, schemaClass);
             String rowKey = bigTableRowKeyParser.parse(sinkConfig.getRowKeyTemplate(), message);
-            RowMutationEntry rowMutationEntry = RowMutationEntry
-                    .create(rowKey)
-                    .setCell("family-test", "odpf-message", parsedOdpfMessage.toString());
+            RowMutationEntry rowMutationEntry = RowMutationEntry.create(rowKey);
+            ParsedOdpfMessage parsedOdpfMessage = odpfMessageParser.parse(message, modeAndSchema.getFirst(), modeAndSchema.getSecond());
+            bigTableSchema.getColumnFamilies().forEach(
+                    columnFamily -> bigTableSchema
+                            .getColumns(columnFamily)
+                            .forEach(column -> {
+                                String fieldName = bigTableSchema.getField(columnFamily, column);
+                                String value = String.valueOf(parsedOdpfMessage.getFieldByName(fieldName, schema));
+                                rowMutationEntry.setCell(columnFamily, column, value);
+                            }));
             return new BigTableRecord(rowMutationEntry, index, null, true);
         } catch (Exception e) {
             ErrorInfo errorInfo = new ErrorInfo(e, ErrorType.DESERIALIZATION_ERROR);
