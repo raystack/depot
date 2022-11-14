5aea5b8e40e61c79fcf70504bab26fe9974d4160
jesrypandawa <jesry.p.pandawa@gmail.com>
2022-11-15 01:10:05 +0700

    fix: add validation for url config

--- src/main/java/io/odpf/depot/http/request/RequestFactory.java
+++ src/main/java/io/odpf/depot/http/request/RequestFactory.java
@@ -1,18 +1,22 @@
 package io.odpf.depot.http.request;
 
 import io.odpf.depot.config.HttpSinkConfig;
+import io.odpf.depot.exception.ConfigurationException;
 import io.odpf.depot.http.enums.HttpRequestMethodType;
 import io.odpf.depot.http.request.body.RequestBody;
 import io.odpf.depot.http.request.body.RequestBodyFactory;
 import io.odpf.depot.http.request.builder.HeaderBuilder;
 import io.odpf.depot.http.request.builder.UriBuilder;
 import io.odpf.depot.http.enums.HttpRequestType;
+import org.apache.commons.lang3.StringUtils;
 
 public class RequestFactory {
 
     public static Request create(HttpSinkConfig config) {
+        String serviceUrl = config.getSinkHttpServiceUrl();
+        validateUrlConfig(serviceUrl);
+        UriBuilder uriBuilder = new UriBuilder(serviceUrl);
         HeaderBuilder headerBuilder = new HeaderBuilder(config.getSinkHttpHeaders());
-        UriBuilder uriBuilder =  new UriBuilder(config.getSinkHttpServiceUrl());
         HttpRequestMethodType httpMethod = config.getSinkHttpRequestMethod();
         RequestBody requestBody = RequestBodyFactory.create(config);
 
@@ -20,6 +24,12 @@ public class RequestFactory {
             return new SingleRequest(httpMethod, headerBuilder, uriBuilder, requestBody);
         } else {
             return new BatchRequest();
+        }
+    }
+
+    private static void validateUrlConfig(String serviceUrl) {
+        if (serviceUrl == null || StringUtils.isEmpty(serviceUrl)) {
+            throw new ConfigurationException("Service URL '" + serviceUrl + "' is invalid");
         }
     }
 }
--- src/test/java/io/odpf/depot/http/request/RequestFactoryTest.java
+++ src/test/java/io/odpf/depot/http/request/RequestFactoryTest.java
@@ -1,11 +1,14 @@
 package io.odpf.depot.http.request;
 
 import io.odpf.depot.config.HttpSinkConfig;
+import io.odpf.depot.exception.ConfigurationException;
 import io.odpf.depot.http.enums.HttpRequestBodyType;
 import io.odpf.depot.http.enums.HttpRequestMethodType;
 import io.odpf.depot.http.enums.HttpRequestType;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -14,11 +17,15 @@ import org.mockito.junit.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class RequestFactoryTest {
 
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     @Mock
     private HttpSinkConfig sinkConfig;
 
     @Test
     public void shouldReturnSingleRequest() {
+        Mockito.when(sinkConfig.getSinkHttpServiceUrl()).thenReturn("http://dummy.com");
         Mockito.when(sinkConfig.getRequestType()).thenReturn(HttpRequestType.SINGLE);
         Mockito.when(sinkConfig.getRequestBodyType()).thenReturn(HttpRequestBodyType.RAW);
         Mockito.when(sinkConfig.getSinkHttpRequestMethod()).thenReturn(HttpRequestMethodType.PUT);
@@ -28,10 +35,27 @@ public class RequestFactoryTest {
 
     @Test
     public void shouldReturnBatchRequest() {
+        Mockito.when(sinkConfig.getSinkHttpServiceUrl()).thenReturn("http://dummy.com");
         Mockito.when(sinkConfig.getRequestType()).thenReturn(HttpRequestType.BATCH);
         Mockito.when(sinkConfig.getRequestBodyType()).thenReturn(HttpRequestBodyType.RAW);
         Mockito.when(sinkConfig.getSinkHttpRequestMethod()).thenReturn(HttpRequestMethodType.PUT);
         Request request = RequestFactory.create(sinkConfig);
         Assert.assertTrue(request instanceof BatchRequest);
+    }
+
+    @Test
+    public void shouldFailWhenUrlConfigIsEmpty() {
+        expectedException.expect(ConfigurationException.class);
+        expectedException.expectMessage("Service URL '' is invalid");
+        Mockito.when(sinkConfig.getSinkHttpServiceUrl()).thenReturn("");
+        RequestFactory.create(sinkConfig);
+    }
+
+    @Test
+    public void shouldFailWhenUrlConfigIsNull() {
+        expectedException.expect(ConfigurationException.class);
+        expectedException.expectMessage("Service URL 'null' is invalid");
+        Mockito.when(sinkConfig.getSinkHttpServiceUrl()).thenReturn(null);
+        RequestFactory.create(sinkConfig);
     }
 }
