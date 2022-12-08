f215c7f8b371fcd7ca5298dd912669727e93b0fe
Lavkesh Lahngir <lavkesh51@gmail.com>
2022-12-08 14:51:46 +0800

    Http batch request (#70)
    * feat: add batch request
    
    * chore: minor refactor
    
    Co-authored-by: jesrypandawa <jesry.p.pandawa@gmail.com>

--- src/main/java/io/odpf/depot/http/request/BatchRequest.java
+++ src/main/java/io/odpf/depot/http/request/BatchRequest.java
@@ -1,6 +1,5 @@
 package io.odpf.depot.http.request;
 
-import io.odpf.depot.error.ErrorInfo;
 import io.odpf.depot.error.ErrorType;
 import io.odpf.depot.exception.ConfigurationException;
 import io.odpf.depot.exception.DeserializerException;
@@ -17,7 +16,6 @@ import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -47,33 +45,23 @@ public class BatchRequest implements Request {
                 String body = requestBody.build(messages.get(index));
                 validBodies.put(index, body);
             } catch (EmptyMessageException e) {
-                records.add(createErrorRecord(e, ErrorType.INVALID_MESSAGE_ERROR, index, message.getMetadata()));
+                records.add(RequestUtils.createErrorRecord(e, ErrorType.INVALID_MESSAGE_ERROR, index, message.getMetadata()));
             } catch (ConfigurationException | IllegalArgumentException e) {
-                records.add(createErrorRecord(e, ErrorType.UNKNOWN_FIELDS_ERROR, index, message.getMetadata()));
+                records.add(RequestUtils.createErrorRecord(e, ErrorType.UNKNOWN_FIELDS_ERROR, index, message.getMetadata()));
             } catch (DeserializerException | IOException e) {
-                records.add(createErrorRecord(e, ErrorType.DESERIALIZATION_ERROR, index, message.getMetadata()));
+                records.add(RequestUtils.createErrorRecord(e, ErrorType.DESERIALIZATION_ERROR, index, message.getMetadata()));
             }
         }
-        List<Integer> validRecordsIndex = new ArrayList<>(validBodies.keySet());
-        List<String> validRecordsBody = new ArrayList<>(validBodies.values());
-        if (validRecordsBody.size() != 0) {
+        if (validBodies.size() != 0) {
             Map<String, String> requestHeaders = headerBuilder.build();
-            URI requestUrl = uriBuilder.build(Collections.emptyMap());
+            URI requestUrl = uriBuilder.build();
             HttpEntityEnclosingRequestBase request = RequestMethodFactory.create(requestUrl, httpMethod);
             requestHeaders.forEach(request::addHeader);
-            request.setEntity(buildEntity(validRecordsBody.toString()));
-            HttpRequestRecord validRecord = new HttpRequestRecord(null, true, request);
-            validRecordsIndex.forEach(validRecord::addIndex);
+            request.setEntity(RequestUtils.buildStringEntity(validBodies.values()));
+            HttpRequestRecord validRecord = new HttpRequestRecord(request);
+            validRecord.addAllIndexes(validBodies.keySet());
             records.add(validRecord);
         }
         return records;
-    }
-
-    private HttpRequestRecord createErrorRecord(Exception e, ErrorType type, Integer index, Map<String, Object> metadata) {
-        ErrorInfo errorInfo = new ErrorInfo(e, type);
-        log.error("Error while parsing record for message. Metadata : {}, Error: {}", metadata, errorInfo);
-        HttpRequestRecord record = new HttpRequestRecord(errorInfo, false, null);
-        record.addIndex(index);
-        return record;
     }
 }
--- src/main/java/io/odpf/depot/http/request/Request.java
+++ src/main/java/io/odpf/depot/http/request/Request.java
@@ -2,16 +2,10 @@ package io.odpf.depot.http.request;
 
 import io.odpf.depot.http.record.HttpRequestRecord;
 import io.odpf.depot.message.OdpfMessage;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
 
 import java.util.List;
 
 public interface Request {
-
     List<HttpRequestRecord> createRecords(List<OdpfMessage> messages);
 
-    default StringEntity buildEntity(String stringBody) {
-        return new StringEntity(stringBody, ContentType.APPLICATION_JSON);
-    }
 }
